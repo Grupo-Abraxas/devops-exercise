858978a54411d4eb3c5bfd5bba6c057b355aa8ab
KielRodriguez <kr.rdz.20@gmail.com>
2018-07-30 15:52:05 -0500

    counter implementation to posts requests

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+count = 0
+
+@app.route("/", methods=['GET', 'POST'])
 def hello():
+    if request.method == 'POST':
+       global count
+       count += 1
     return "Hello World!!!"
 
+@app.route("/counter")
+def counter():
+    return 'Post requests: %s' % str(count)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
