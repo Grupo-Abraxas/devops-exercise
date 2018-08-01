32e9b9f76d79606079683d9cd24bf10ffcfb6ac2
KielRodriguez <kr.rdz.20@gmail.com>
2018-08-01 17:17:46 -0500

    Add documentation

--- app.py
+++ app.py
@@ -6,13 +6,26 @@ count = 0
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
+    """
+    Método maneja comportamiento para las 
+    entradas GET y POST del root
+
+    POST Retorna un Hello World e incrementa contador
+    GET  Retorna un Hello World
+    """
     if request.method == 'POST':
        global count
        count += 1
-    return "Hello World!!!"
+    return 'Hello World %s!!!' % request.method
 
 @app.route("/counter")
 def counter():
+    """
+    Método maneja comportamiento para la entrada
+    GET del endpoint counter
+
+    GET  Retorna en numero de reques al endpoit root por POST
+    """
     return 'Post requests: %s' % str(count)
 
 if __name__ == "__main__":
